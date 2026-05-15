0a69cce00afbb28229133657c7968c164b20aca0
Isaac Bailey <isaac.daniel.bailey@proton.me>
2026-05-15 13:07:20 -0500

    Add github deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+import process from 'node:process'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: process.env.VITE_BASE || '/',
   plugins: [
     react(),
     tailwindcss(),
